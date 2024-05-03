c5537cf5cb5ca1e23fe77d12a4993fd02a7be67f
tyagushev_evm <s.y.tyagushev@evm.eco>
2024-05-03 13:29:46 +0300

    evm blk

--- MCU_CAN_AV/ViewModels/LimitsViewModel.cs
+++ MCU_CAN_AV/ViewModels/LimitsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Messaging;
+using CommunityToolkit.Mvvm.Input;
 using CsvHelper;
 using DynamicData;
 using MCU_CAN_AV.Devices;
@@ -12,52 +13,119 @@ using System.Linq;
 using System.Reactive.Subjects;
 using System.Text;
 using System.Threading.Tasks;
+using AsyncSocketTest;
+using System.Collections;
+using System.Data.Common;
 
 namespace MCU_CAN_AV.ViewModels
 {
-    internal partial class LimitsViewModel : ObservableValidator, IEnableLogger, IRecipient<ConnectionState>
+    internal partial class LimitsViewModel : ObservableRecipient, IEnableLogger, IRecipient<ConnectionState>
     {
+        System.Timers.Timer? timer;
+        ServerModbusTCP ? RxConnection;
+
+        private void ReadEvmState() { 
+            
+
+            
+        }
+
+        public LimitsViewModel() {
+            Messenger.RegisterAll(this);
+        }    
+
         public void Receive(ConnectionState message)
         {
             if (message.state == ConnectionState.State.Connected)
             {
-                DisposableSpeed = IDevice.Current.OutSpeed.Value.Subscribe((_) =>
+                this.Log().Info("LimitsViewModel Connected");
+
+                timer = new System.Timers.Timer(100);
+                timer.Elapsed += async (_, __) =>
                 {
-                    if (SpeedLim_enable)
-                    {
-                        double res;
 
-                        if (Double.TryParse(SpeedLim, out res) == false) return;
-
-                        if (Math.Abs(_) > res)
-                        {
-                            IDevice.Current.Stop();
-
-                            ((Subject<IDeviceFault>)IDevice.Current.DeviceFaults).OnNext(new BaseDeviceFault("VISU_FLT: Speed"));
+                    if (EVMblk_enable == false) {
+                        EVM_State = "Disabled";
+                        return;
+                    } 
+                    try {
+                        if (RxConnection == null) {
+                            RxConnection = new ServerModbusTCP("localhost", 8888);
                         }
+
+                        if (!RxConnection.Connected)
+                        {
+                            RxConnection = new ServerModbusTCP("localhost", 8888);
+                        }
+
+                       
+
+                        var res = await RxConnection.ReadHoldingsAsync(1, 1, 1);
+
+                        BitArray bits = new BitArray(BitConverter.GetBytes(res[0]));
+                        
+                        if (bits[0] == true) {
+                            timer.Interval = 100;
+                            EVM_State = "Run";
+                        }
+
+                        if (bits[1] == true)
+                        {
+                            EVM_State = "Rdy";
+                            timer.Interval = 1000;
+                            IDevice.Current.Stop();
+                        }
+
+                        if (bits[2] == true)
+                        {
+                            EVM_State = "Fault";
+                            timer.Interval = 1000;
+                            IDevice.Current.Stop();
+                        }
+
+                        
                     }
-                });
+                    catch (Exception ex) {
+                        RxConnection = null;
+                      //  this.Log().Warn(ex.Message.ToString());
+                        EVM_State = "not connected";
+                        timer.Interval = 1000;
+                    }
+                   
+                };
+
+                timer?.Start();
+
             }
 
             if (message.state == ConnectionState.State.Disconnected)
             {
+                RxConnection?.close();
                 DisposableSpeed?.Dispose();
+                timer?.Stop();
+                timer?.Dispose();
             }
         }
 
         [ObservableProperty]
-        [ValidateUint2()]
+     //   [ValidateUint2()]
         private string _voltageLim = "0";
 
         [ObservableProperty]
         private bool _voltageLim_enable = false;
 
         [ObservableProperty]
-        [ValidateUint2()]
+    //    [ValidateUint2()]
         private string _speedLim = "0";
 
         [ObservableProperty]
         private bool _speedLim_enable = false;
+
+        [ObservableProperty]
+        private string _eVM_State = "not connected";
+
+        [ObservableProperty]
+        private bool _eVMblk_enable = false;
 
         IDisposable? DisposableSpeed;
 
