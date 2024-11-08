40cc25e369df2f8c9f09bf65ffdec2f462e28d5b
tyagushev_evm <s.y.tyagushev@evm.eco>
2024-11-08 17:17:15 +0300

    ms

---
+++ MCU_CAN_AV/Views/MultiScope.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MCU_CAN_AV;
+
+public partial class MultiScope : UserControl
+{
+    public MultiScope()
+    {
+        InitializeComponent();
+    }
+}
